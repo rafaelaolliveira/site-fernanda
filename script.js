e220efbb0384a653d4c507cfaedbb35f7f5b1b66
Rafaela Oliveira <rafaelaolliveira01@gmail.com>
2026-06-25 16:36:46 -0300

    menuatt

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function{
-    
-}
+const mobileBtn = document.querySelector('.btn');
+const navLinks = document.getElementById ('links');
+const icon = document.querySelector('.btn i');
+
+mobileBtn.addEventListener('click', () => {
+navLinks.classList.toggle('show');
+icon.classList.toggle('fa-times');
+icon.classList.toggle('fa-bars');
+})
+
